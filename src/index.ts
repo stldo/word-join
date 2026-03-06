b5e63423f6c2609a71623a9c05da3bdbdd3415b6
stldo <git@stldo.dev>
2026-03-06 19:43:21 -0300

    refactor: use tsdown and biome

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,36 @@
-export default function wordJoin (
+export default function wordJoin(
   words: unknown[] = [],
   options: {
-    conjunction?: string
-    oxford?: boolean
-    separator?: string
-  } = {}
+    conjunction?: string;
+    oxford?: boolean;
+    separator?: string;
+  } = {},
 ): string {
-  const wordCount = words.length
+  const wordCount = words.length;
 
   if (wordCount === 0) {
-    return ''
+    return "";
   } else if (wordCount === 1) {
-    return `${words[0]}`
+    return `${words[0]}`;
   }
 
-  const conjunction = options.conjunction || 'and'
+  const conjunction = options.conjunction || "and";
 
   if (wordCount === 2) {
-    return `${words[0]} ${conjunction} ${words[1]}`
+    return `${words[0]} ${conjunction} ${words[1]}`;
   }
 
-  const lastIndex = wordCount - 1
-  const separator = options.separator || ','
+  const lastIndex = wordCount - 1;
+  const separator = options.separator || ",";
 
-  let start = words[0]
+  let start = words[0];
 
   for (let index = 1; index < lastIndex; index++) {
     /* skip first (1) and last (lastIndex) words */
-    start += `${separator} ${words[index]}`
+    start += `${separator} ${words[index]}`;
   }
 
-  const end = ` ${conjunction} ${words[lastIndex]}`
+  const end = ` ${conjunction} ${words[lastIndex]}`;
 
-  return options.oxford
-    ? `${start}${separator}${end}`
-    : `${start}${end}`
+  return options.oxford ? `${start}${separator}${end}` : `${start}${end}`;
 }
